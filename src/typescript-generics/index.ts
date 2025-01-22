7a75f34c6b9fa4bf4316b4f9e37063aa91d578b0
aman mulani <mulaniaman0504@gmail.com>
2025-01-22 09:35:38 +0530

    feat:generics are awesome 🤩

--- src/typescript-generics/index.ts
+++ src/typescript-generics/index.ts
@@ -1,13 +1,24 @@
 export default function index() {
-    const useState<T> = (initialValue: T): [getter:()=> T, setter:(str:T)=> void] => {
-        let value = initialValue;
-    
-        const getter = () => value;
-    
-        const setter = (str: T) => (value = str);
-    
-        return [getter, setter];
-      };
-}
+  const useState = <T>(
+    initialValue: T
+  ): [getter: () => T, setter: (str: T) => void] => {
+    let value = initialValue;
 
-const [st1Getter,st2Getter] = useState<string>('hello')
+    const getter = () => value;
+
+    const setter = (str: T) => (value = str);
+
+    return [getter, setter];
+  };
+
+  const [st1Getter, st1Setter] = useState<string>("str1");
+  const [st2Getter, st2Setter] = useState<number>(1);
+
+  // console.log(st1Getter());
+  // st1Setter("str2");
+  // console.log(st1Getter());
+
+  // console.log(st2Getter());
+  // st2Setter(2);
+  // console.log(st2Getter());
+}
