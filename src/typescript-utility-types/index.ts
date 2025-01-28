144d3770507b7cf5226e7cf5f961e6043b91d33c
aman mulani <mulaniaman0504@gmail.com>
2025-01-28 08:47:34 +0530

    feat: Pick Utility Type 🪮

--- src/typescript-utility-types/index.ts
+++ src/typescript-utility-types/index.ts
@@ -7,12 +7,15 @@ export default function index() {
     email?: string;
   }
 
-  //1. partial it takes the type and make everything in it as optional
+  //1. Required ::  it takes the type and make everything in it as optional
   type IMyUserOptions = Partial<IMyUser>;
 
-  // 2. required :: this takes a interface and makes all the field required and remove optionals;
-
+  // 2. Required :: it takes a interface and makes all the field required and remove optionals;
   type IMyUserRequired = Required<IMyUser>;
+
+  // 3. Pick :: it takes an interface and some keys of it and generat a new type of them;
+  // hint:: use | or to add multiple keys
+  type IMyUserWithNameAndID = Pick<IMyUser, "id" | "name">;
 
   const merge = (user: IMyUser, overrides: IMyUserRequired): IMyUser => {
     return {
