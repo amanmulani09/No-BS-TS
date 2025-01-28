7304bb144448b70bc42f71c1f6610aec28ed792b
aman mulani <mulaniaman0504@gmail.com>
2025-01-28 08:42:53 +0530

    feat: utitility type required

--- src/typescript-utility-types/index.ts
+++ src/typescript-utility-types/index.ts
@@ -10,16 +10,18 @@ export default function index() {
   //1. partial it takes the type and make everything in it as optional
   type IMyUserOptions = Partial<IMyUser>;
 
-  // 2. required
+  // 2. required :: this takes a interface and makes all the field required and remove optionals;
 
-  const merge = (user: IMyUser, overrides: IMyUserOptions): IMyUser => {
+  type IMyUserRequired = Required<IMyUser>;
+
+  const merge = (user: IMyUser, overrides: IMyUserRequired): IMyUser => {
     return {
       ...user,
       ...overrides,
     };
   };
 
-  console.log(
-    merge({ name: "avi", id: "420", email: "avimail" }, { email: "ravimail" })
-  );
+  //   console.log(
+  //     merge({ name: "avi", id: "420", email: "avimail" }, { email: "ravimail" })
+  //   );
 }
