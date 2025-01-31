62c11e9dcaac87b696699eb8efb7aec9ebdc83d4
aman mulani <mulaniaman0504@gmail.com>
2025-01-31 09:31:01 +0530

    feat: numeric literal type

--- src/typescript-enums-literals/index.ts
+++ src/typescript-enums-literals/index.ts
@@ -8,5 +8,18 @@ export default function index() {
   const isLoading = (state: loadingStage): boolean =>
     state === loadingStage.loading;
 
-  console.log(isLoading(loadingStage.beforeLoad));
+  //   console.log(isLoading(loadingStage.beforeLoad));
+
+  //literal types;
+
+  const rollDice = (dice: 1 | 2 | 3 ):number {
+    let pip = 0;
+
+    for(let i=0; i<dice; i++){
+        pip =  Math.floor(Math.random() * 5) + 1;
+    }
+    return pip
+  }
+
+  console.log(rollDice(2))
 }
